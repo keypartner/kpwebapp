1f05bce053fc053a9439128c468677c715f68893
Valerio Lollobrigida <valeriolollo@gmail.com>
2016-05-11 15:56:54 +0200

    Added missing annotations in EJB KeyEmployeeDAO

--- src/main/java/it/keyp/webapp/ejb/KeyEmployeeDAO.java
+++ src/main/java/it/keyp/webapp/ejb/KeyEmployeeDAO.java
@@ -2,11 +2,15 @@ package it.keyp.webapp.ejb;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import it.keyp.webapp.interceptors.Logging;
 import it.keyp.webapp.domain.KeyEmployee;
 
+@Stateless
+@Logging
 public class KeyEmployeeDAO implements KeyEmployeeDAOLocal {
 	
 	@PersistenceContext(unitName = "MyPU")
